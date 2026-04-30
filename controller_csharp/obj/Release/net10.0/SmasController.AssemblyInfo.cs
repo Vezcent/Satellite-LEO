fdfd3f232209e48c3b6bdd648388884a84c32162
Lirya <68703541+Vezcent@users.noreply.github.com>
2026-04-30 19:19:54 +0700

    fix

--- controller_csharp/obj/Release/net10.0/SmasController.AssemblyInfo.cs
+++ controller_csharp/obj/Release/net10.0/SmasController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmasController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40186714ae4af8e21233a72cb70cc724fc5fdd4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ad38e2a357a220503318036149a1af7762c277")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmasController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmasController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
